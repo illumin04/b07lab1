65d376f874638d7ead51ec496bed3abaaee10ab8
Longqirui Chen <longqiruichen@LongqiruideMacBook-Pro.local>
2023-10-01 14:31:41 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -2,12 +2,23 @@ public class Driver {
 	public static void main(String [] args) {
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+		double [] c1 = {1,1,1};
+		int [] cc = {1,2,4};
+		Polynomial p1 = new Polynomial(c1,cc);
+		System.out.println(p1.evaluate(3));
+		double [] c2 = {1,1,0,1};
+		int [] cc1= {1,6,4,3};
+		Polynomial p2 = new Polynomial(c2,cc1);
+		double []t1= {1,1};
+		int []tt1= {1,2};
+		double []t2= {2,3};
+		int []tt2= {1,3};
+		Polynomial s1= new Polynomial(t1,tt1);
+		Polynomial s2=new Polynomial(t2,tt2);
+		Polynomial ss=s1.multiply(s2);
+		System.out.println(ss.evaluate(2));
 		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
+		System.out.println("s(1) = " + s.evaluate(2));
 		if(s.hasRoot(1))
 			System.out.println("1 is a root of s");
 		else
--- Polynomial.java
+++ Polynomial.java
@@ -1,7 +1,6 @@
-import java.util.Arrays;
-
 public class Polynomial{
 	private double[] coefficients;
+	private int[] powers;
 	
 	private double power(double x,int y) {
 		double result=1.0;
@@ -13,38 +12,92 @@ public class Polynomial{
 	}
 	public Polynomial() {
 		this.coefficients = new double[]{0.0};
+		this.powers = new int[]{0};
 	}
 	
 	
-	public Polynomial(double[] coefficients) {
-		this.coefficients = new double [coefficients.length];
-		for(int i=0;i<coefficients.length;i++)
-		{
-			this.coefficients[i]=coefficients[i];
-		}
-	}
+	public Polynomial(double[] coefficients, int[] powers) {
+        this.coefficients = coefficients.clone();
+        this.powers = powers.clone();
+    }
 	
 	
-	public Polynomial add(Polynomial link) {
-		int size=Math.max(this.coefficients.length,link.coefficients.length);
-		double[] result=new double [size];
-		for(int i=0;i<this.coefficients.length;i++)
-		{
-			result[i]+=this.coefficients[i];
-		}
-		for(int i=0;i<link.coefficients.length;i++)
-		{
-			result[i]+=link.coefficients[i];
-		}
-		return new Polynomial(result);
-	}
+	public Polynomial add(Polynomial other) {
+        int maxLength = Math.max(this.coefficients.length, other.coefficients.length);
+        double[] resultCoefficients = new double[1000005];
+        int[] resultExponents = new int[1000005];
+
+        for (int i = 0; i < this.coefficients.length; i++) {
+            double coeff = this.coefficients[i];
+            int exp = this.powers[i];
+            int index = indexOfExponent(other.powers, exp);
+
+            if (index != -1) {
+                resultCoefficients[i] = coeff + other.coefficients[index];
+                resultExponents[i] = exp;
+            } else {
+                resultCoefficients[i] = coeff;
+                resultExponents[i] = exp;
+            }
+        }
+
+        for (int i = 0; i < other.coefficients.length; i++) {
+            double coeff = other.coefficients[i];
+            int exp = other.powers[i];
+            if (indexOfExponent(resultExponents, exp) == -1) {
+                resultCoefficients[maxLength] = coeff;
+                resultExponents[maxLength] = exp;
+                maxLength++;
+            }
+        }
+
+        return new Polynomial(resultCoefficients, resultExponents);
+    }
+
+    private int indexOfExponent(int[] array, int exp) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == exp) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 	
-	
+    public Polynomial multiply(Polynomial other) {
+        int maxLength = this.coefficients.length + other.coefficients.length;
+        double[] resultCoefficients = new double[1000005];
+        int[] resultExponents = new int[1000005];
+
+        for (int i = 0; i < this.coefficients.length; i++) {
+            double coeff1 = this.coefficients[i];
+            int exp1 = this.powers[i];
+
+            for (int j = 0; j < other.coefficients.length; j++) {
+                double coeff2 = other.coefficients[j];
+                int exp2 = other.powers[j];
+
+                int exp = exp1 + exp2;
+                double coeff = coeff1 * coeff2;
+                int index = indexOfExponent(resultExponents, exp);
+
+                if (index != -1) {
+                    resultCoefficients[index] += coeff;
+                } else {
+                    resultCoefficients[maxLength] = coeff;
+                    resultExponents[maxLength] = exp;
+                    maxLength++;
+                }
+            }
+        }
+
+        return new Polynomial(resultCoefficients, resultExponents);
+    }
 	public double evaluate(double x) {
 		double sum=0.0;
 		for(int i=0;i<coefficients.length;i++)
 		{
-			sum+=coefficients[i]*power(x,i);
+			sum+=coefficients[i]*power(x,powers[i]);
 		}
 		return sum;
 	}
